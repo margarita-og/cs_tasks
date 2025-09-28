da294a31e4289a3c13baec6ba919ddfe21d51297
unknown <mavagan7@gmail.com>
2025-09-28 14:36:00 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вводите числа по одному, чтобы просуммировать их");
+            string input = Console.ReadLine();
+            int sum = 0;
+            while (input != "0")
+            {
+                int number = int.Parse(input);
+                sum += number;
+                Console.WriteLine(sum);
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
